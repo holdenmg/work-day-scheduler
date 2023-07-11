1bbb7cd9fbe4cc222c22f8a4839d1006bc729f88
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-07-11 17:25:02 -0400

    Added function to change clas sof schedule block based on current hour. Linked advanced dayjs in order to utilize ordinals

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-//saveButton = $('#saveBtn');
+
+
+
 
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  
+  //Listener for 
   $( ".saveBtn" ).on( "click", function(event) {
     var section = $(event.target).closest("section");
     var key = section.attr("id");
@@ -27,7 +21,9 @@ $(document).ready(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
@@ -40,6 +36,31 @@ function displaySched () {
     textArea.val(text);
 })
 }
+
+$("section").each(function(){
+  id = $(this).attr("id");
+  blockHour = parseInt(id.slice(-2));
+  console.log(blockHour);
+  var nowHour = parseInt(dayjs().format('H'));
+
+  console.log(nowHour);
+  if(nowHour > blockHour){
+    $(this).removeClass("present")
+    $(this).removeClass("future") 
+    $(this).addClass("past");
+  }
+  else if(nowHour === blockHour){
+    $(this).removeClass("past")
+    $(this).removeClass("future") 
+    $(this).addClass("present");
+  }
+  else{
+    $(this).removeClass("present")
+    $(this).removeClass("pastt") 
+    $(this).addClass("future");
+  }
+
+});
   // TODO: Add code to display the current date in the header of the page.
   
 var rightNow = dayjs().format('dddd, MMMM D');
