22b111af96cf26b6be68424600a86814fe746bb1
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-07-11 18:45:51 -0400

    Added a different message text conditional for saved confirmation. Added comments, updated formating

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 
 $(document).ready(function () {
+  $("#saved-confirmation").hide()
   
-//var advancedFormat = require('dayjs/plugin/advancedFormat');
-//dayjs.extend(advancedFormat);
-  //Listener for 
+
+  //Listener for save button. On click stores text in local storage, uses id as key
   $( ".saveBtn" ).on( "click", function(event) {
     var section = $(event.target).closest("section");
     var key = section.attr("id");
@@ -14,60 +14,53 @@ $(document).ready(function () {
     var text = textArea.val();
     console.log(text)
     localStorage.setItem(key, text);
-    //alert(localStorage.getItem(key))
-  });
-
-// TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  
-
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-function displaySched () {
+  //show and then fade out saved confirmation message. If text was left blank or deleted, message will say "...Updated in..." vice "...Added to..."
+    if(text){
+      $("#saved-confirmation").html("Appointment Added to <span style='color:magenta'> localStorage </span>&check;")  
+    }
+    else{
+      $("#saved-confirmation").html("Appointment Updated in <span style='color:magenta'> localStorage </span>&check;")
+    }
+    $("#saved-confirmation").show()
+    $("#saved-confirmation" ).fadeOut( 2500, function() {
+    })
+  })
+  //Populate schedule with items saved in local storage
+  function displaySched () {
   $("section").each(function(){
     key = $(this).attr("id");
     textArea = $(this).find("textarea");
     text = localStorage.getItem(key);
     textArea.val(text);
-})
-}
-
-$("section").each(function(){
-  id = $(this).attr("id");
-  blockHour = parseInt(id.slice(-2));
-  console.log(blockHour);
-  var nowHour = parseInt(dayjs().format('H'));
-
-  console.log(nowHour);
-  if(nowHour > blockHour){
-    $(this).removeClass("present")
-    $(this).removeClass("future") 
-    $(this).addClass("past");
-  }
-  else if(nowHour === blockHour){
+  })
+  }   
+  //Goes to each date block section and updates style
+  $("section").each(function(){
+    id = $(this).attr("id");
+  //Gets number portion of id and turns into int for comparison to time
+    blockHour = parseInt(id.slice(-2));
+    var nowHour = parseInt(dayjs().format('H'));
+//Compare current time and block time and update style as neccessary
+    if(nowHour > blockHour){
+      $(this).removeClass("present")
+     $(this).removeClass("future") 
+      $(this).addClass("past");
+    }
+    else if(nowHour === blockHour){
     $(this).removeClass("past")
     $(this).removeClass("future") 
     $(this).addClass("present");
-  }
-  else{
-    $(this).removeClass("present")
-    $(this).removeClass("pastt") 
-    $(this).addClass("future");
-  }
-
+    }
+    else{
+      $(this).removeClass("present")
+      $(this).removeClass("pastt") 
+      $(this).addClass("future");
+    }
 });
-  // TODO: Add code to display the current date in the header of the page.
-  
+//Display current time in header
 var rightNow = dayjs().format('dddd, MMMM Do');
 $("#currentDay").text(rightNow);
-displaySched()
-  
+displaySched() 
 });
 
 
